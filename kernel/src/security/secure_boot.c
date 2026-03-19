e04b1ac1da7aee8c08bf44a0b627a054f0955504
Divyang Panchasara <divyang4481@gmail.com>
2026-03-20 04:58:30 +0530

    Add HAL interfaces for secure boot trust and key isolation

--- kernel/src/security/secure_boot.c
+++ kernel/src/security/secure_boot.c
@@ -56,6 +56,46 @@ hal_board_get_boot_policy(void) {
   return &g_default_boot_policy;
 }
 
+__attribute__((weak)) int hal_secure_boot_get_measurements(
+    bharat_boot_measurement_t *out_measurements, size_t *inout_count) {
+  (void)out_measurements;
+  (void)inout_count;
+  return -1;
+}
+
+__attribute__((weak)) int hal_secure_boot_verify_measurements(
+    const bharat_boot_measurement_t *measurements, size_t count,
+    bharat_trust_evidence_t *out_evidence) {
+  (void)measurements;
+  (void)count;
+  (void)out_evidence;
+  return -1;
+}
+
+__attribute__((weak)) int
+hal_secure_mem_restrict_region(const bharat_secure_region_t *region) {
+  (void)region;
+  return -1;
+}
+
+__attribute__((weak)) int
+hal_secure_mem_release_region(const bharat_secure_region_t *region) {
+  (void)region;
+  return -1;
+}
+
+__attribute__((weak)) int hal_secure_crypto_dma_window_config(
+    const bharat_crypto_dma_window_t *window) {
+  (void)window;
+  return -1;
+}
+
+__attribute__((weak)) int
+hal_secure_crypto_dma_window_clear(const bharat_crypto_dma_window_t *window) {
+  (void)window;
+  return -1;
+}
+
 const bharat_boot_policy_t *bharat_boot_active_policy(void) {
   return hal_board_get_boot_policy();
 }
@@ -73,4 +113,26 @@ int bharat_secure_boot_stage_hook(bharat_boot_stage_t stage,
                                   uint64_t measurement) {
   return bharat_audit_record(BHARAT_AUDIT_EVENT_BOOT_MEASURE, 0U, 0,
                              (uint64_t)stage, measurement);
+}
+
+int bharat_secure_key_region_lock(uint64_t base, uint64_t size,
+                                  uint32_t flags) {
+  bharat_secure_region_t region = {
+      .base = base,
+      .size = size,
+      .flags = flags | BHARAT_SECURE_REGION_ZERO_ON_RELEASE,
+  };
+
+  return hal_secure_mem_restrict_region(&region);
+}
+
+int bharat_secure_key_region_unlock(uint64_t base, uint64_t size,
+                                    uint32_t flags) {
+  bharat_secure_region_t region = {
+      .base = base,
+      .size = size,
+      .flags = flags,
+  };
+
+  return hal_secure_mem_release_region(&region);
 }
