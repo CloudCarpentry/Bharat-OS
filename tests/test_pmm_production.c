4110632ae65fd308abc2f3df76a48a4662e93a89
Divyang Panchasara <divyang4481@gmail.com>
2026-03-23 14:58:24 +0530

    Restore historical coverage and benchmark tables in memory report

--- tests/test_pmm_production.c
+++ tests/test_pmm_production.c
@@ -3,11 +3,6 @@
 #include <assert.h>
 #include <string.h>
 
-void kernel_panic(const char* msg) {
-    printf("PANIC: %s\n", msg);
-    abort();
-}
-
 // Mock kernel definitions for PMM test harness
 #include "../../kernel/include/mm/pmm.h"
 #include "../../kernel/include/mm.h"
