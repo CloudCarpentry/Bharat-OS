bd85ed5d47ed5ee36a6fe8050f9b832459da0abc
Divyang Panchasara <divyang4481@gmail.com>
2026-04-15 11:43:45 +0530

    Improve QEMU harness portability and arm32 linkage (#536)

---
+++ arch/riscv/riscv32/platform/qemu_virt/boot/early_console.c
@@ -0,0 +1,11 @@
+#include "boot/platform_boot_info.h"
+
+void platform_riscv32_qemu_virt_early_init(platform_boot_info_t *plat) {
+    if (!plat) {
+        return;
+    }
+
+    plat->has_early_console = true;
+    plat->uart_phys_base = 0x10000000ULL;
+    plat->uart_type = BHARAT_EARLY_UART_TYPE_NS16550;
+}
