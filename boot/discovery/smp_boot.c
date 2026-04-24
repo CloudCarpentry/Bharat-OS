25c166d00dc4b2f15b05c5a2f69a4136c33a7c51
Divyang Panchasara <divyang4481@gmail.com>
2026-04-24 13:15:49 +0530

    refactor(boot): migrate compatibility wrappers to symlinked boot dirs

--- boot/discovery/smp_boot.c
+++
@@ -1,2 +0,0 @@
-/* Legacy compatibility translation unit. Canonical source moved to core/boot/discovery/smp_boot.c. */
-#include "../../core/boot/discovery/smp_boot.c"
--- boot/include/boot/boot_validate.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-/* Legacy compatibility wrapper. Canonical header moved to core/boot/include/boot/boot_validate.h. */
-#include "../../../core/boot/include/boot/boot_validate.h"
