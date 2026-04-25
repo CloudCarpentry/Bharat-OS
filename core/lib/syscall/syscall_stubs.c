8a13278d2ed0423803285ce49ec5c04a4f786241
Divyang Panchasara <divyang4481@gmail.com>
2026-04-25 12:50:30 +0530

    Implement arch syscall dispatch in userspace stubs (#732)

--- core/lib/syscall/syscall_stubs.c
+++ core/lib/syscall/syscall_stubs.c
@@ -1,4 +1,15 @@
 #include <bharat/uapi/syscall_nr.h>
+#include <bharat/uapi/sys_errno.h>
+
+#if defined(__x86_64__)
+#include <bharat/uapi/arch/x86_64/syscall.h>
+#elif defined(__aarch64__)
+#include <bharat/uapi/arch/arm64/syscall.h>
+#elif defined(__riscv)
+#include <bharat/uapi/arch/riscv64/syscall.h>
+#else
+#error "Unsupported architecture"
+#endif
 
 long bharat_syscall(long sysno,
                     long arg1,
@@ -7,12 +18,9 @@ long bharat_syscall(long sysno,
                     long arg4,
                     long arg5,
                     long arg6) {
-    (void)sysno;
-    (void)arg1;
-    (void)arg2;
-    (void)arg3;
-    (void)arg4;
-    (void)arg5;
-    (void)arg6;
-    return -1;
+    if (sysno < 0 || (unsigned long)sysno > (unsigned long)SYSCALL_MAX) {
+        return -(long)SYS_ENOSYS;
+    }
+
+    return bharat_syscall_arch(sysno, arg1, arg2, arg3, arg4, arg5, arg6);
 }
